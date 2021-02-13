060d7125297cb8ced52fb8bab90caf0420070275
Stuart Cullen <stuart@clarity-composites.co.uk>
2021-02-13 15:25:26 +0000

    Initial commit with a basic spring boot setup

---
+++ src/main/java/com/stuartcullen/Stockopediatestv2/StockopediaTestV2Application.java
@@ -0,0 +1,13 @@
+package com.stuartcullen.Stockopediatestv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockopediaTestV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockopediaTestV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stuartcullen/Stockopediatestv2/StockopediaTestV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuartcullen.Stockopediatestv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockopediaTestV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
